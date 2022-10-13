e0e521f2ec33f7b75fcefbf3b5d2e35ba1d676a3
olarte18 <snheider2224656@correo.uis.edu.co>
2022-10-12 22:09:14 -0500

    Codigo v/1.1

--- dado_for.py
+++ dado_for.py
@@ -1,27 +1,33 @@
 import random
 n=int(input("cuantas veces lanzará el dado: "))
-c1=0
-c2=0
-c3=0
-c4=0
-c5=0
-c6=0
+c1=""
+c2=""
+c3=""
+c4=""
+c5=""
+c6=""
 
 for i in range (n):
     d= random.randint(1,6)
     if d==1:
-        c1=c1+1
+        c1=c1+"*"
     elif d==2:
-        c2=c2+1
+        c2=c2+"*"
     elif d==3:
-        c3=c3+1
+        c3=c3+"*"
     elif d==4:
-        c4=c4+1
+        c4=c4+"*"
     elif d==5:
-        c5=c5+1
+        c5=c5+"*"
     elif d==6:
-        c6=c6+1
+        c6=c6+"*"
 
-print(c1,c2,c3,c4,c5,c6)
+print("Cara Numero 1: ",c1)
+print("Cara Numero 2: ",c2)
+print("Cara Numero 3: ",c3)
+print("Cara Numero 4: ",c4)
+print("Cara Numero 5: ",c5)
+print("Cara Numero 6: ",c6)
+
 
 
